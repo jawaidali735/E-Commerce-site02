7669a7658e40fd391dc0f916b671873267a0da04
Jawaid Ali <jawaidaliupdates@gmail.com>
2024-12-09 04:58:27 +0500

    first commit

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -12,6 +12,10 @@ const config: Config = {
         background: "var(--background)",
         foreground: "var(--foreground)",
       },
+
+      fontFamily:{
+        josefin: ["Josefin Sans","Arial", "sans-serif"]
+      },
     },
   },
   plugins: [],
